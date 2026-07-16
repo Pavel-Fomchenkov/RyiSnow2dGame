450a62233e213c3be8ca55deeb22f44f0e7cb5da
Pavel-Fomchenkov <pavelf.skypro@gmail.com>
2026-07-16 21:05:10 +0300

    Possibly fix for a bug that allows a sleeping sprites remains after sleep

--- src/main/UI.java
+++ src/main/UI.java
@@ -1069,8 +1069,8 @@ public class UI {
                 counter = 0;
                 gp.eManager.lighting.dayState = gp.eManager.lighting.day;
                 gp.eManager.lighting.dayCounter = 0;
-                gp.gameState = gp.playState;
                 gp.player.getPlayerImage();
+                gp.gameState = gp.playState;
             }
         }
     }
